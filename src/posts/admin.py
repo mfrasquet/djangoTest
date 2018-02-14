50fc9f93e6c9db8f7e1364e0ceeee8914b4bf7e2
Miguel <mfrasquetherraiz@gmail.com>
2018-02-14 23:04:20 +0100

    admin tunned

--- src/posts/admin.py
+++ src/posts/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 from posts.models import Post
 # Register your models here.
 
-admin.site.register(Post)
+class PostModelAdmin(admin.ModelAdmin):
+    list_display=["title","updated","timestamp"]
+    list_display_links=["updated"]
+    list_filter=["updated","timestamp"]
+    search_fields=["content","title"]
+    class Meta:
+        model=Post
+
+admin.site.register(Post, PostModelAdmin)
